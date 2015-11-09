2fb1eb7fd6cab42ba34b76fdb99623a7c3b3bfa9
ZeronTheXeon <apgthexeon@gmail.com>
2015-11-08 20:21:01 -0800

    Push to Engine pt. 2

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -1,13 +1,12 @@
 ﻿namespace Engine
 {
-    public class Player
+    public class Player : LivingThing
     {
-        public int CurrentHP { get; set; }
-        public int MaxHP { get; set; }
         public int CurrentMP { get; set; }
         public int MaxMP { get; set; }
         public int Money { get; set; }
         public int Level { get; set; }
-        public int Location { get; set; }
+
+        public string Location { get; set; }
     }
 }
--- SimpleRPG/SimpleRPG.cs
+++ SimpleRPG/SimpleRPG.cs
@@ -1,37 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Engine;
+﻿using Engine;
+using System;
 using System.Windows.Forms;
 
 namespace SimpleRPG
 {
     public partial class SimpleRPG : Form
     {
-        private Player player1 = new Player(); 
+        private Player player1 = new Player();
+
         public SimpleRPG()
         {
             InitializeComponent();
+
+            player1.CurrentHP = 100;
+            player1.CurrentMP = 0;
+            player1.Level = 1;
+            player1.Location = "Home";
+            player1.MaxHP = 100;
+            player1.MaxMP = 100;
+            player1.Money = 20;
+
+            GoldValue.Text = player1.Money.ToString();
+            HealthValue.Text = player1.CurrentHP.ToString() + " / " + player1.MaxHP.ToString();
+            CharLevelValue.Text = player1.Level.ToString();
+            ManaValue.Text = player1.CurrentMP.ToString() + " / " + player1.MaxMP.ToString();
+
+            LocationValue.Text = player1.Location;
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
-
         }
 
         private void CharStats_Click(object sender, EventArgs e)
         {
-            MoneyValue.Text = "1";
         }
 
         private void SimpleRPG_Load(object sender, EventArgs e)
         {
+        }
 
+        private void Money_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void MoneyValue_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void CharLevelValue_Click(object sender, EventArgs e)
+        {
         }
     }
 }
